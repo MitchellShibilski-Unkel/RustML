1f024219f8bcaec43d00e708c1a2bde57f827c75
Mitchell Shibilski-Unkel <mitchellshibs@gmail.com>
2024-11-26 11:05:05 -0600

    Added examples

---
+++ examples/demo2.rs
@@ -0,0 +1,9 @@
+mod rustml;
+
+fn main() {
+    let RML = rustml::RustML;
+    let test: [f64; 4] = [1.0, 2.0, 3.0, 4.0];
+    let testy: [f64; 4] = [5.0, 6.0, 7.0, 8.0];
+    let testw: [f64; 4] = [55.0, 66.0, 77.0, 88.0];
+    println!("{:?}", RML.RNN(&test, &testy, &testw, 1.0));
+}
